cf8d212941bd4395bb40e348238a3fe03c50ea57
John Kolden <john.kolden@gmail.com>
2018-02-09 13:20:55 -0800

    simplified percent failure query

--- news.py
+++ news.py
@@ -8,23 +8,24 @@ DBNAME = "news"
 
 def sql_helper(query):
     """helper function to run sql queries"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
     try:
+        db = psycopg2.connect(database=DBNAME)
+        c = db.cursor()
         query = c.execute(query)
         results = c.fetchall()
-    except Exception:
-        print "The SQL query could not be executed"
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
     db.close
     return results
 
 
 def top_articles():
     """print the most downloaded articles"""
-    query = ("select count, title from (select count(l.*) count, " +
-             "a.title from log l, articles a where l.path != '/' and " +
-             "'/article/' || a.slug = l.path group by path, title "
-             "order by count desc) as foo limit 3")
+    query = ("""SELECT count(l.*) count, a.title
+                FROM log l, articles a
+                WHERE '/article/' || a.slug = l.path
+                GROUP BY path, title
+                ORDER BY count desc limit 3""")
     articles = sql_helper(query)
     print("The three most popular articles are:")
     for views, title in articles:
@@ -33,10 +34,12 @@ def top_articles():
 
 def popular_authors():
     """print the most popular authors"""
-    query = ("select auth.name, count(l.*) count " +
-             "from log l join articles a on '/article/' || a.slug = l.path " +
-             "left join authors auth ON a.author=auth.id " +
-             "group by auth.name order by count desc")
+    query = ("""SELECT auth.name, count(l.*) count
+                FROM log l
+                JOIN articles a on '/article/' || a.slug = l.path
+                LEFT JOIN authors auth ON a.author=auth.id
+                GROUP BY auth.name
+                ORDER BY count desc""")
     authors = sql_helper(query)
     print("The most downloaded authors are:")
     for author, views in authors:
@@ -46,17 +49,16 @@ def popular_authors():
 def fails():
     """find days when download errors exceeded 1 percent of the /
     total downloads for that day"""
-    query = ("select d, (fail * 1.0 / (success + fail) * 1.0) * 100 pct " +
-             "from (SELECT cast(time as date) d, " +
-             "SUM(CASE status WHEN '200 OK' THEN 1 ELSE 0 END) " +
-             "AS success, SUM(CASE status WHEN '404 NOT FOUND' " +
-             "THEN 1 ELSE 0 END) AS fail " +
-             "from log group by d) as foo " +
-             "where (fail * 1.0 / (success + fail) * 1.0) * 100 > 1")
+    query = ("""SELECT d, (fail * 1.0 / total) * 100 pct
+                FROM (SELECT cast(time as date) d,
+                count(*) AS total, SUM(CASE status WHEN '404 NOT FOUND'
+                THEN 1 ELSE 0 END) AS fail
+                FROM log group by d) as foo
+                where ((fail * 1.0 / total) * 1.0) * 100 > 1""")
     fails = sql_helper(query)
     print("Days when more than 1% of downloads led to errors:")
     for date, pct in fails:
-        print('{} - {:.2f}% errors'.format(date.strftime("%B %d, %Y"), pct))
+        print('{:%B %d, %Y} - {:.2f}% errors'.format(date, pct))
 
 
 if __name__ == "__main__":
